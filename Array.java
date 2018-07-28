707bbbd2bdd2c8496d72ab49624e59182862643d
vk222 <vp372@njit.edu>
2018-07-28 01:01:30 -0400

    Create Array.java

---
+++ Array.java
@@ -0,0 +1,19 @@
+import java.util.Arrays;
+import java.util.*;
+import java.lang.reflect.*;
+public class Array {
+
+	public static void main(String[] args) {
+		int[] numbers = {10, 40, 60, 20, 80,100,};
+		 System.out.println("index 4: " + numbers[4]);
+		 numbers[5] = 55;
+		 System.out.println("index 5:" + numbers[5]);
+		 System.out.println("lenght of number array:"+numbers.length);
+		
+		 
+		
+		
+
+	}
+
+}
